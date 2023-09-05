55700ea861b3b33505f1291345499f207c2bb541
Harish A <harish18092002@gmail.com>
2023-09-05 21:13:23 +0530

    Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ServicesComponent } from './services/services.component';
+import { CertificationComponent } from './certification/certification.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: AppComponent,
+  children: [
+    { path: 'header', component: HeaderComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'achievements', component: CertificationComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: 'header' }
+
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificationComponent } from './certification/certification.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServicesComponent,
+    SkillsComponent,
+    CertificationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
