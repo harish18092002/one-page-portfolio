54f2caf69a6b9131d830058d63447bc362b9e89f
Harish A <harish18092002@gmail.com>
2023-09-22 23:08:02 +0530

    skills

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -6,36 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
-  certificates = [
+  projects = [
     {
-      title: 'Certificate 1',
+      title: 'Project 1',
       imageUrl: '../../assets/certificates/pals.jpg',
-      description: 'Description for Certificate 1'
+      description: 'Description for Project 1'
     },
     {
-      title: 'Certificate 2',
+      title: 'Project 2',
       imageUrl: '../../assets/certificates/Wc intern_page-0001.jpg',
-      description: 'Description for Certificate 2'
+      description: 'Description for Project 2'
     },
     {
-      title: 'Certificate 3',
+      title: 'Project 3',
       imageUrl: '../../assets/certificates/prathyusha.jpg',
-      description: 'Description for Certificate 3'
+      description: 'Description for Project 3'
     },
     {
-      title: 'Certificate 4',
+      title: 'Project 4',
       imageUrl: '../../assets/certificates/guvi ai.png',
-      description: 'Description for Certificate 4'
+      description: 'Description for Project 4'
     },
-    {
-      title: 'Certificate 5',
-      imageUrl: '../../assets/certificates/webinar certificate_page-0001.jpg',
-      description: 'Description for Certificate 5'
-    },
-    {
-      title: 'Certificate 6',
-      imageUrl: '../../assets/certificates/Guvi world record.png',
-      description: 'Description for Certificate 6'
-    },
+
+
   ];
+
+
 }
