d62f7ece6b7ac0bdff8d9e73742ee592431558d4
Harish A <harish18092002@gmail.com>
2023-09-24 20:21:31 +0530

    projects

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -9,23 +9,27 @@ export class ServicesComponent {
   projects = [
     {
       title: 'Project 1',
-      imageUrl: '../../assets/certificates/pals.jpg',
-      description: 'Description for Project 1'
+      imageUrl: '../../assets/Projects/Screenshot 2023-09-24 193528.png',
+      description: 'Description for Project 1',
+      link: 'https://github.com/harish18092002/Network.git'
     },
     {
       title: 'Project 2',
-      imageUrl: '../../assets/certificates/Wc intern_page-0001.jpg',
-      description: 'Description for Project 2'
+      imageUrl: '../../assets/Projects/Screenshot 2023-09-22 203036.png',
+      description: 'Description for Project 2',
+      link: ''
     },
     {
       title: 'Project 3',
-      imageUrl: '../../assets/certificates/prathyusha.jpg',
-      description: 'Description for Project 3'
+      imageUrl: '../../assets/Projects/API project.png',
+      description: 'Description for Project 3',
+      link: 'https://github.com/harish18092002/secret-of-happiness1.git'
     },
     {
       title: 'Project 4',
-      imageUrl: '../../assets/certificates/guvi ai.png',
-      description: 'Description for Project 4'
+      imageUrl: '../../assets/Projects/potfolio.png',
+      description: 'Description for Project 4',
+      link: 'https://portfolio18092002.web.app/home'
     },
 
 
