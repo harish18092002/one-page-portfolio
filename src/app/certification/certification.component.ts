0fe3d94916e14e9f5b0b2ade708a61ac7ce2b443
Harish A <harish18092002@gmail.com>
2023-10-07 21:45:17 +0530

    project

--- src/app/certification/certification.component.ts
+++ src/app/certification/certification.component.ts
@@ -37,51 +37,7 @@ export class CertificationComponent {
       imageUrl: '../../assets/certificates/Guvi world record.png',
       description: 'Description for Certificate 6'
     },
-    // {
-    //   title: 'Certificate 7',
-    //   imageUrl: '../../assets/certificates/Gl android app dev_page-0001.jpg',
-    //   description: 'Description for Certificate 7'
-    // },
-    // {
-    //   title: 'Certificate 8',
-    //   imageUrl: '../../assets/certificates/Internshala app dev_page-0001.jpg',
-    //   description: 'Description for Certificate 8'
-    // },
-    // {
-    //   title: 'Certificate 9',
-    //   imageUrl: '../../assets/certificates/gl Android studio_page-0001.jpg',
-    //   description: 'Description for Certificate 9'
-    // },
-    // {
-    //   title: 'Certificate 10',
-    //   imageUrl: '../../assets/certificates/Internshala Core Java_page-0001.jpg',
-    //   description: 'Description for Certificate 10'
-    // },
-    // {
-    //   title: 'Certificate 11',
-    //   imageUrl: '../../assets/certificates/gl ethical hack_page-0001.jpg',
-    //   description: 'Description for Certificate 11'
-    // },
-    // {
-    //   title: 'Certificate 12',
-    //   imageUrl: '../../assets/certificates/Internshala Ethical Hacking_page-0001.jpg',
-    //   description: 'Description for Certificate 12'
-    // },
-    // {
-    //   title: 'Certificate 13',
-    //   imageUrl: '../../assets/certificates/GL Mobile pentesting_page-0001.jpg',
-    //   description: 'Description for Certificate 13'
-    // },
-    // {
-    //   title: 'Certificate 14',
-    //   imageUrl: '../../assets/certificates/Internshala Internship & Job Preparation_page-0001.jpg',
-    //   description: 'Description for Certificate 14'
-    // },
-    // {
-    //   title: 'Certificate 15',
-    //   imageUrl: '../../assets/certificates/Internshala non-tech intern_page-0001.jpg',
-    //   description: 'Description for Certificate 15'
-    // },
+
   ];
 }
 
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -10,26 +10,30 @@ export class ServicesComponent {
     {
       title: 'Project 1',
       imageUrl: '../../assets/Projects/Screenshot 2023-09-24 193528.png',
-      description: 'Description for Project 1',
+      description: 'An Angular-powered Customer Contact Management System, offering secure login with UUID and efficient contact storage via local storage, with seamless pagination for enhanced usability',
+      technologies: 'Angular,Firebase',
       link: 'https://github.com/harish18092002/Network.git'
     },
     {
       title: 'Project 2',
       imageUrl: '../../assets/Projects/Screenshot 2023-09-22 203036.png',
-      description: 'Description for Project 2',
-      link: ''
+      description: 'A dynamic website with real-time full-screen color changing functionality, adding vibrancy to your online experience',
+      link: '',
+      technologies: 'HTML,CSS,JS'
     },
     {
       title: 'Project 3',
       imageUrl: '../../assets/Projects/API project.png',
-      description: 'Description for Project 3',
-      link: 'https://github.com/harish18092002/secret-of-happiness1.git'
+      description: 'Built three Node.js APIs for PostgreSQL using HTTP, Postman, and Insomnia: one retrieves records, the second creates and inserts new entries into the database, enhancing data management capabilities.',
+      link: 'https://github.com/harish18092002/secret-of-happiness1.git',
+      technologies: 'PostgreSQL,HTTP, Postman,NodeJs'
     },
     {
       title: 'Project 4',
       imageUrl: '../../assets/Projects/potfolio.png',
       description: 'Description for Project 4',
-      link: 'https://portfolio18092002.web.app/home'
+      link: 'https://portfolio18092002.web.app/home',
+      technologies: 'Angular,Firebase,NodeJs'
     },
 
 
