d406d33165b5b5821e3543cc473941463afbcc91
Harish A <harish18092002@gmail.com>
2023-11-21 09:48:15 +0530

    alt for seo

--- src/app/certification/certification.component.ts
+++ src/app/certification/certification.component.ts
@@ -10,32 +10,42 @@ export class CertificationComponent {
     {
       title: 'Certificate 1',
       imageUrl: '../../assets/certificates/pals.jpg',
-      description: 'Description for Certificate 1'
+      description: 'Description for Certificate 1',
+      alt: 'Certificate of being a finalist in the PALS Innowah competition conducted by IIT Madras'
     },
     {
       title: 'Certificate 2',
       imageUrl: '../../assets/certificates/Wc intern_page-0001.jpg',
-      description: 'Description for Certificate 2'
+      description: 'Description for Certificate 2',
+      alt: 'Certificate of being completing my internship'
+
     },
     {
       title: 'Certificate 3',
       imageUrl: '../../assets/certificates/prathyusha.jpg',
-      description: 'Description for Certificate 3'
+      description: 'Description for Certificate 3',
+      alt: 'Certificate of being Participating in Hackathon'
+
     },
     {
       title: 'Certificate 4',
       imageUrl: '../../assets/certificates/guvi ai.png',
-      description: 'Description for Certificate 4'
+      description: 'Description for Certificate 4',
+      alt: 'Certificate of being Participating in Guvi AI hackathon'
+
     },
     {
       title: 'Certificate 5',
       imageUrl: '../../assets/certificates/webinar certificate_page-0001.jpg',
-      description: 'Description for Certificate 5'
+      description: 'Description for Certificate 5',
+      alt: 'Certificate of attending webinar on AI and Machine Learning'
+
     },
     {
       title: 'Certificate 6',
       imageUrl: '../../assets/certificates/Guvi world record.png',
-      description: 'Description for Certificate 6'
+      description: 'Description for Certificate 6',
+      alt: 'Certificate of Guvi World Record'
     },
 
   ];
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -12,28 +12,35 @@ export class ServicesComponent {
       imageUrl: '../../assets/Projects/Screenshot 2023-09-24 193528.png',
       description: 'An Angular-powered Customer Management System, offering secure login with UUID and efficient contact storage, with seamless pagination',
       technologies: 'Angular,Firebase',
-      link: 'https://github.com/harish18092002/Network.git'
+      link: 'https://github.com/harish18092002/Network.git',
+      alt: 'Screenshot of Project 1 - An Angular-powered Customer Management System with secure login, contact storage, and pagination'
     },
     {
       title: 'Project 2',
       imageUrl: '../../assets/Projects/Screenshot 2023-09-22 203036.png',
       description: 'A dynamic website with real-time full-screen color changing functionality, adding vibrancy to your online experience',
       link: '',
-      technologies: 'HTML,CSS,JS'
+      technologies: 'HTML,CSS,JS',
+      alt: 'Screenshot of Project 2 - A dynamic website with real-time full-screen color changing functionality, enhancing your online experience with HTML, CSS, and JS'
+
     },
     {
       title: 'Project 3',
       imageUrl: '../../assets/Projects/API project.png',
       description: 'Built three Node.js APIs for PostgreSQL using HTTP, Postman, and Insomnia: one retrieves records, the second creates and inserts new entries into the database, enhancing data management capabilities.',
       link: 'https://github.com/harish18092002/secret-of-happiness1.git',
-      technologies: 'PostgreSQL,HTTP, Postman,NodeJs'
+      technologies: 'PostgreSQL,HTTP, Postman,NodeJs',
+      alt: 'Screenshot of Project 3 - Built three Node.js APIs for PostgreSQL using HTTP, Postman, and Insomnia, enhancing data management capabilities with PostgreSQL, HTTP, Postman, and Node.js'
+
     },
     {
       title: 'Project 4',
       imageUrl: '../../assets/Projects/potfolio.png',
       description: 'Hosted on Firebase, my portfolio website showcases my skills and features my resume, providing a dynamic online presence to highlight my expertise and experiences.',
       link: 'https://github.com/harish18092002/Healthcare-Hackathon.git',
-      technologies: 'Angular,Firebase,NodeJs'
+      technologies: 'Angular,Firebase,NodeJs',
+      alt: 'Screenshot of Project 4 - Hosted on Firebase, my portfolio website showcases skills and features my resume, providing a dynamic online presence with Angular, Firebase, and Node.js'
+
     },
 
     {
@@ -41,7 +48,9 @@ export class ServicesComponent {
       imageUrl: '../../assets/Projects/emids.png',
       description: " I've developed a secure healthcare admin site, enabling doctors and nurses to efficiently manage patient records",
       link: 'https://portfolio18092002.web.app/home',
-      technologies: 'Angular,Uuid,NodeJs'
+      technologies: 'Angular,Uuid,NodeJs',
+      alt: 'Screenshot of Project 5 - I\'ve developed a secure healthcare admin site, enabling doctors and nurses to efficiently manage patient records, using Angular, Uuid, and Node.js.'
+
     }
 
 
